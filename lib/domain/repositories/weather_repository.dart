7ed2ec6f6db1b157de26dee1399f5858991f119a
Przybylski Kamil <przybylskikamil663@gmail.com>
2022-02-02 22:36:29 +0100

    add loc & name

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -1,9 +1,22 @@
+import 'package:dio/dio.dart';
 import 'package:simple_weather/domain/models/weather_model.dart';
 
 class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
-    return const WeatherModel(city: 'Warsaw', temperature: -5.5);
+    // http://api.weatherapi.com/v1/current.json?key=af6f4fff1e894844a56145545222801&q=Bartoszyce&aqi=no
+    final response = await Dio().get<Map<String, dynamic>>(
+        'http://api.weatherapi.com/v1/current.json?key=af6f4fff1e894844a56145545222801&q=$city&aqi=no');
+
+    final responseData = response.data;
+    if (responseData == null) {
+      return null;
+    }
+
+    final name = responseData['location']['name'] as String;
+    final temperature = (responseData['current']['temp_c'] + 0.0) as double;
+
+    return WeatherModel(city: name, temperature: temperature);
   }
 }
